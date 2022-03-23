e32cf1d6c02f3a85981e8448f8254cc12d83a2b9
Neel Kothari <neelkothariak@gmail.com>
2022-03-23 09:03:02 +0530

    first commit

---
+++ lib/models/recipe.api.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:recipe_list_app/models/recipe.dart';
+import 'package:http/http.dart' as http;
+
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list',
+        {"limit": "24", "start": "0", "tag": "list.recipe.popular"});
+    final response = await http.get(uri, headers: {
+      "X-RapidAPI-Host": "yummly2.p.rapidapi.com",
+      "X-RapidAPI-Key": "c8746204d3msh34f0f2ab45a3b92p13f091jsndfa3b17d241b",
+      "useQueryString": "true"
+    });
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['feed']) {
+      _temp.add(i['content']['details']);
+    }
+
+    return Recipe.recipesFromSnapshot(_temp);
+  }
+}
---
+++ lib/views/home.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_list_app/views/widgets/recipe_card.dart';
+
+import '../models/recipe.api.dart';
+import '../models/recipe.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late List<Recipe> _recipes;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    getRecipes();
+  }
+
+  Future<void> getRecipes() async {
+    _recipes = await RecipeApi.getRecipe();
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.restaurant_menu),
+              SizedBox(width: 30),
+              Text('Food Recipe'),
+            ],
+          ),
+        ),
+        body: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: _recipes.length,
+                itemBuilder: (context, index) {
+                  return RecipeCard(
+                      title: _recipes[index].name,
+                      cookTime: _recipes[index].totalTime,
+                      rating: _recipes[index].rating.toString(),
+                      thumbnailUrl: _recipes[index].images);
+                },
+              ));
+  }
+}
